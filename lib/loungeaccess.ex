f429d81f499a1407c610531104f5986d58625244
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-04-15 19:57:21 -0400

    feature: check user status and issue ticket if applicable

--- lib/loungeaccess.ex
+++ lib/loungeaccess.ex
@@ -1,18 +1,23 @@
 defmodule Loungeaccess do
-  @moduledoc """
-  Documentation for `Loungeaccess`.
-  """
+  use Application
+  alias UUID
 
-  @doc """
-  Hello world.
+  def start(_type, _args) do
+    Loungeaccess.main()
+    Supervisor.start_link([], strategy: :one_for_one)
+  end
 
-  ## Examples
+  def main do
+    name = Enum.random([:Anderson, :Erin, :Bruno])
+    status = Enum.random([:Silver, :Gold, :Platinum])
+    ticket = UUID.uuid4() |> String.split("-") |> List.first()
 
-      iex> Loungeaccess.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    if status === :Silver do
+      IO.puts("\nSorry #{name}, #{status} members are not allowed to enter this lounge.\n")
+    else
+      IO.puts("\nWelcome to the lounge, #{name}.")
+      IO.puts("Thank you for being a #{status} member!")
+      IO.puts("Ticket: #{ticket}\n")
+     end
   end
 end
